953609a79c987f5232baadedea9939fb25b945a5
anjirajardula <anjirajardula@gmail.com>
2021-09-27 10:24:53 +0530

    Added:Welcome to the review problem

---
+++ src/review.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class review {
+
+    public static void main(String[] args) {
+        int n = 6;
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+
+
